a1e91e89f502a2c05734292ab1ddce13e6a605bf
Sambo Chea <sombochea@cubetiqs.com>
2025-07-17 20:40:54 +0700

    Updated logos and styling

--- frappe/integrations/doctype/social_login_key/social_login_key.py
+++ frappe/integrations/doctype/social_login_key/social_login_key.py
@@ -178,7 +178,7 @@ class SocialLoginKey(Document):
 			"provider_name": "Frappe",
 			"enable_social_login": 1,
 			"custom_base_url": 1,
-			"icon": "/assets/frappe/images/frappe-favicon.svg",
+			"icon": "/assets/frappe/images/cubis-favicon.svg",
 			"redirect_url": "/api/method/frappe.integrations.oauth2_logins.login_via_frappe",
 			"api_endpoint": "/api/method/frappe.integrations.oauth2.openid_profile",
 			"api_endpoint_args": None,
